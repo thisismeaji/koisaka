d1a723506fc476fc48e239801943ac947143f89b
thisismeaji <thisismeaji20@gmail.com>
2025-01-28 16:34:52 +0700

    add footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styles from "../footer/footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={Styles.footer}>
+        <p>© 2025 Leotic.id. Semua Hak Cipta Dilindungi.</p>
+    </footer>
+  )
+}
