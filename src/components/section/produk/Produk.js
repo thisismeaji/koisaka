5168f3967a34c445f462e5e16570ddd1916c5c8a
thisismeaji <thisismeaji20@gmail.com>
2025-01-29 09:00:17 +0700

    add produk section

--- src/components/section/produk/Produk.js
+++ src/components/section/produk/Produk.js
@@ -4,15 +4,68 @@ import Image from "next/image";
 
 export default function Produk() {
   return (
-    <section>
-        <div className={Styles.cardProduk}>
-            <div>
-                <Image 
-                    src="/assets/sb-hudielong-platinum.png" 
-                    alt="Foto Profil" 
-                    width={300} 
-                    height={300} 
-                />
+    <section className={Styles.produkSection}>
+        <h2>Produk kami</h2>
+        <div className={Styles.card}>
+            <div className={Styles.cardProduk}>
+                <div className={Styles.gambar}>
+                    <Image 
+                        src="/assets/sb-hudielong-platinum.png" 
+                        alt="Foto Profil" 
+                        width={512} 
+                        height={512} 
+                    />
+                </div>
+                <div className={Styles.diskripsi}>
+                    <h3>SB Hudielong / Butterfly Platinum grade A</h3>
+                    <p>Koi Bulat premium dengan warna platinum yang berkilau.</p>
+                    <h4>Rp.200.000</h4>
+                </div>
+            </div>
+            <div className={Styles.cardProduk}>
+                <div className={Styles.gambar}>
+                    <Image 
+                        src="/assets/sb-hudielong-platinum.png" 
+                        alt="Foto Profil" 
+                        width={512} 
+                        height={512} 
+                    />
+                </div>
+                <div className={Styles.diskripsi}>
+                    <h3>SB Hudielong / Butterfly Platinum grade A</h3>
+                    <p>Koi Bulat premium dengan warna platinum yang berkilau.</p>
+                    <h4>Rp.200.000</h4>
+                </div>
+            </div>
+            <div className={Styles.cardProduk}>
+                <div className={Styles.gambar}>
+                    <Image 
+                        src="/assets/sb-hudielong-platinum.png" 
+                        alt="Foto Profil" 
+                        width={512} 
+                        height={512} 
+                    />
+                </div>
+                <div className={Styles.diskripsi}>
+                    <h3>SB Hudielong / Butterfly Platinum grade A</h3>
+                    <p>Koi Bulat premium dengan warna platinum yang berkilau.</p>
+                    <h4>Rp.200.000</h4>
+                </div>
+            </div>
+            <div className={Styles.cardProduk}>
+                <div className={Styles.gambar}>
+                    <Image 
+                        src="/assets/sb-hudielong-platinum.png" 
+                        alt="Foto Profil" 
+                        width={512} 
+                        height={512} 
+                    />
+                </div>
+                <div className={Styles.diskripsi}>
+                    <h3>SB Hudielong / Butterfly Platinum grade A</h3>
+                    <p>Koi Bulat premium dengan warna platinum yang berkilau.</p>
+                    <h4>Rp.200.000</h4>
+                </div>
             </div>
         </div>
     </section>
